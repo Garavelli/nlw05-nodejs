d6c006384dcf9eba6114918ef3f772bb2524981b
Garavelli <maycongaravelli@hotmail.com>
2021-05-04 22:06:06 -0300

    'h'

--- client/nlw0-client/components/ChatBox.jsx
+++ client/nlw0-client/components/ChatBox.jsx
@@ -1,9 +1,164 @@
 import React from 'react';
 import theme from '../theme';
+import io from 'socket.io-client'
 
 // import { Container } from './styles';
+function InfosForChatBox ({ setInfosParams, infosParams, handleClick }) {
+
+  return (
+    <>
+      <section className="chatContainer__main">
+        <div className="initialMessage">
+          <label htmlFor="message">How can we help you?</label>
+          <textarea name="message" id="message" required onChange={(e) => {
+            const newParams = infosParams;
+            newParams.text = e.target.value;
+            setInfosParams(newParams)            
+          }}></textarea>
+        </div>
+
+        <div className="emailForInit">          
+          <label htmlFor="email">Your e-mail</label>
+          <input type="text" name="email" id="email" required onChange={(e) => {
+            const newParams = infosParams;
+            newParams.email = e.target.value;
+            setInfosParams(newParams)            
+          }}/>
+        </div>
+      </section>
+
+      <section className="chatContainer__sendArea">
+        <button className="btnChat red">Cancel</button>
+        <button className="btnChat purple" onClick={(e) => handleClick(e)}>Start</button>
+      </section>
+      <style jsx>
+        {`
+          .chatContainer__main {            
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              flex-basis: 100%;
+              justify-content: space-evenly; 
+              width: 100%;
+              font-size: 1.2rem;
+
+                          
+            
+            .initialMessage {
+              display: flex;
+              flex-direction: column;
+              width: 100%;
+              margin: 1rem 0;
+
+              textarea {
+                width: 100% !important;
+                height: 9rem;
+                border-radius: ${theme.nicets.borderRadius};
+                resize: none;
+                padding: 0.7rem
+              }                
+            }
+          }
+          .emailForInit {
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            margin: 1rem 0;
+            
+            input {
+              padding: 0.7rem;
+              border: 1px solid gray;
+              border-radius: ${theme.nicets.borderRadius};
+            }
+          }
+          .chatContainer__sendArea{
+              width: 100%;
+              display: flex;
+              align-items: center;
+              justify-content: flex-end;
+              gap: 10px;
+
+              .btnChat {
+                padding: 0.6rem 1.3rem;
+                color: white;
+                font-weight: bold;
+                border: none;
+                border-radius: ${theme.nicets.borderRadius};
+                cursor: pointer;                
+              }
+
+              .btnChat:hover {
+                filter: brightness(1.4);
+              }
+            }             
+        `}
+      </style>
+    </>
+  )
+}
+
+function ChatForChatBox () {
+  return (
+    <>
+      <div className="initialMessage">
+        <label htmlFor="message">fasdfasdfasdfasdfasdfasdfasdfasdfasdfsadfasdfasdfasdfasdfdfdfdfdfdfdfsdfdfdfdfdfdf</label>
+        <textarea name="message" id="message"></textarea>
+      </div>
+
+      <div className="emailForInit">          
+        <label htmlFor="email">Your e-mail</label>
+        <input type="text" name="email" id="email"/>
+      </div>     
+    </>
+  )
+}
 
 function ChatBox() {
+  const [screen, setScreen] = React.useState('infos');
+  const [infosParams, setInfosParams] = React.useState({email: '', text: ''});
+  const [errors, setErrors] = React.useState([]);
+
+  function addToAObjState(state, newInfo) {
+    const newState = state;
+    const newID = newState.length > 0 ? newState[newState.length - 1].id + 1 : 1;
+    return [...state, {id: newID, text: newInfo}];
+  };
+
+  function hasError() {
+    let errorsT = [];
+    if (!infosParams.email || !infosParams.text) {
+      
+      
+      if (!infosParams.email) {errorsT = addToAObjState(errorsT, 'Email is empty!')};
+      if (!infosParams.text) {errorsT  = addToAObjState(errorsT, 'Text is empty!')};
+      
+      
+    };
+    setErrors(errorsT)
+
+    return (errorsT.length < 1 ? false : true);
+  };
+
+  function startWebSocket () {
+    const socket = io("ws://localhost:3333");
+    socket.on("connect", () => {
+      socket.emit("client_first_access", infosParams, (call, err) => {
+        if (err) {
+          console.err(err);
+        }else {
+          console.log(call);
+        }
+      })
+    })
+    console.log(e)
+  }
+
+  function handleClick(e) {
+    if (hasError()) return;    
+    setScreen('chat');
+    //startWebSocket();    
+  }
+
   return (
     <div className="chatContainer">
       <section className="chatContainer__header">
@@ -11,25 +166,15 @@ function ChatBox() {
         <div className="closeButton">X</div>
       </section>
 
-      <section className="chatContainer__main">
+      <section className="chatContainer__errors" id="errorSpace">
+        {errors.map((error)=>{
+          return <p key={error.id}>{error.text}</p>;
+        })}
+      </section>  
 
-        <div className="initialMessage">
-          <label htmlFor="message">How can we help you?</label>
-          <textarea name="message" id="message"></textarea>
-        </div>
-
-        <div className="emailForInit">          
-          <label htmlFor="email">Your e-mail</label>
-          <input type="text" name="email" id="email"/>
-        </div>
-
-      </section>
-
-      <section className="chatContainer__sendArea">
-        <button className="btnChat  red">Cancel</button>
-        <button className="btnChat purple">Start</button>
-      </section>
-
+      {screen === 'infos' && <InfosForChatBox setInfosParams={setInfosParams} infosParams={infosParams} handleClick={handleClick} />}
+      {screen === 'chat' && <ChatForChatBox />}      
+    
       <style jsx>
         {`  
           .chatContainer{
@@ -69,65 +214,16 @@ function ChatBox() {
                 background-color: white;
                 cursor: pointer;
               }
-            } // header
+            } // header     
 
-            &__main {
+            &__errors {
+              width: 100%;
+              color: red;
               display: flex;
               flex-direction: column;
-              align-items: center;
-              flex-basis: 100%;
-              justify-content: space-evenly; 
-              width: 100%;
+
               font-size: 1.2rem;
-
-              .initialMessage {
-                display: flex;
-                flex-direction: column;
-                width: 100%;
-                margin: 1rem 0;
-
-                textarea {
-                  width: 100% !important;
-                  height: 10rem;
-                  border-radius: ${theme.nicets.borderRadius};
-                  resize: none;
-                  padding: 0.7rem
-                }                
-              }
-              .emailForInit {
-                display: flex;
-                flex-direction: column;
-                width: 100%;
-                margin: 1rem 0;
-                
-                input {
-                  padding: 0.7rem;
-                  border: 1px solid gray;
-                  border-radius: ${theme.nicets.borderRadius};
-                }
-              }             
-            } //main
-
-            &__sendArea{
-              width: 100%;
-              display: flex;
-              align-items: center;
-              justify-content: flex-end;
-              gap: 10px;
-
-              .btnChat {
-                padding: 0.6rem 1.3rem;
-                color: white;
-                font-weight: bold;
-                border: none;
-                border-radius: ${theme.nicets.borderRadius};
-                cursor: pointer;                
-              }
-
-              .btnChat:hover {
-                filter: brightness(1.4);
-              }
-            }            
+            }       
           }
         `}
       </style>
